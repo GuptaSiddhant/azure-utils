8d781841be67c02ce09938e5d557e8d0c6d6861e
Siddhant Gupta <guptasiddhant@icloud.com>
2025-08-15 15:54:07 +0300

    feat(sbm): refactor permissions handling in storybooks router

--- apps/storybooks-manager/index.js
+++ apps/storybooks-manager/index.js
@@ -1,44 +1,22 @@
+// @ts-check
 import { registerStorybooksRouter } from "@azure-utils/storybooks";
 
-const addRoute = registerStorybooksRouter({});
+const SECRET = process.env["SECRET"];
 
-addRoute("auth", {
-  handler: async (request) => {
-    const principalHeader = request.headers.get("x-ms-client-principal");
-    if (!principalHeader) {
-      return {
-        body: `Unauthorized access. Please provide a valid principal header.`,
-        status: 401,
-      };
+registerStorybooksRouter({
+  checkPermissions: async (permissions, _context, request) => {
+    if (!SECRET) {
+      return true;
     }
 
-    // Decode and parse the claims
-    const decoded = Buffer.from(principalHeader, "base64").toString("utf8");
-    /**
-     * @type {{
-     * claims: { typ: string, val: string }[],
-     * auth_typ: string,
-     * name_typ: string,
-     * role_typ: string,
-     * }}
-     */
-    const clientPrincipal = JSON.parse(decoded);
-    const claims = clientPrincipal?.claims || [];
+    const authSecret = request.headers.get("Authorization");
+    const isValidSecret = authSecret === SECRET;
 
-    const azpToken = claims.find((c) => c.typ === "azp")?.val;
-    if (azpToken) {
-      return {
-        status: 200,
-        jsonBody: { type: "application", clientId: azpToken },
-      };
+    const actions = permissions.map((p) => p.action);
+    if (actions.includes("create") || actions.includes("delete")) {
+      return isValidSecret;
     }
 
-    const name = claims.find((c) => c.typ === "name")?.val;
-    const email = claims.find((c) => c.typ === clientPrincipal.name_typ)?.val;
-    const roles = claims
-      .filter((c) => c.typ === clientPrincipal.role_typ || c.typ === "roles") // role_typ tells the claim type for roles
-      .map((c) => c.val);
-
-    return { status: 200, jsonBody: { type: "user", name, email, roles } };
+    return true;
   },
 });
